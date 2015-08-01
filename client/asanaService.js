dbe9473d944979d9ea0eb1a17a0db592219389be
Ryan Yee <Ryan@Ryans-MacBook-Air.local>
2015-08-01 15:12:46 -0700

    (fix) Removes IIFE from asanaService

--- client/asanaService.js
+++ client/asanaService.js
@@ -7,35 +7,31 @@ app.factory('asanaService', function ($http) {
   var client = Asana.Client.create()
   client.useOauth({ credentials: token })
 
-  (function getTasks () {
-  
-      client.users.me().then(function (user) {
-        var taskParams = {}
-        taskParams.workspaceId = _.first(user.workspaces).id
-        taskParams.userId = user.id
-        return taskParams
-      }).then(function (params) {
-  
-        client.tasks.findAll({
-          assignee: params.userId,
-          workspace: params.workspaceId,
-          limit: 1
-        }).then(function (collection) {
+  client.users.me().then(function (user) {
+    var taskParams = {}
+    taskParams.workspaceId = _.first(user.workspaces).id
+    taskParams.userId = user.id
+    return taskParams
+  }).then(function (params) {
 
-          collection.stream().on('data', function (task) {
+    client.tasks.findAll({
+      assignee: params.userId,
+      workspace: params.workspaceId,
+      limit: 1
+    }).then(function (collection) {
 
-            client.tasks.findById(task.id)
-              .then(function (taskDetails) {
-                TASKLIST.push(taskDetails)
-              })      
+      collection.stream().on('data', function (task) {
 
-          }).on('end', function () {
-            console.log('Captured all tasks')
-          })
-        })
+        client.tasks.findById(task.id)
+          .then(function (taskDetails) {
+            TASKLIST.push(taskDetails)
+          })      
+
+      }).on('end', function () {
+        console.log('Captured all tasks')
       })
-
-  })()
+    })
+  })
 
   return {
     tasks: TASKLIST
