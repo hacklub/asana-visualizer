1ad9e7de7c3062207b6a39dd05bebbd0271eb140
Ryan Yee <Ryan@Ryans-MacBook-Air.local>
2015-08-01 16:33:16 -0700

    (feat) asanaService returns timestamps of completed tasks

--- client/asanaService.js
+++ client/asanaService.js
@@ -1,4 +1,4 @@
-app.factory('asanaService', function ($http) {
+app.factory('asanaService', function ($q) {
 
   // get token from cookie
   var token = document.cookie.split('token=')[1].split(';')[0]
@@ -7,29 +7,47 @@ app.factory('asanaService', function ($http) {
   var client = Asana.Client.create()
   client.useOauth({ credentials: token })
 
+  // TODO take all workspaces, filter by completed by user, map timestamp
+
   client.users.me().then(function (user) {
     var taskParams = {}
-    taskParams.workspaceId = _.first(user.workspaces).id
+    taskParams.workspaces = user.workspaces
+    console.log('user.workspaces',user.workspaces);
     taskParams.userId = user.id
+    console.log("user:", user);
     return taskParams
   }).then(function (params) {
 
-    client.tasks.findAll({
-      assignee: params.userId,
-      workspace: params.workspaceId,
-      limit: 1
-    }).then(function (collection) {
-
-      collection.stream().on('data', function (task) {
-
-        client.tasks.findById(task.id)
-          .then(function (taskDetails) {
-            TASKLIST.push(taskDetails)
-          })      
-
-      }).on('end', function () {
-        console.log('Captured all tasks')
+    var promises = _.map(params.workspaces, function (workspace) {
+      var workspaceTasks = []
+      var deferredCollection = $q.defer()
+      client.tasks.findAll({
+        assignee: params.userId,
+        workspace: workspace.id,
+        limit: 1
+      }).then(function (collection) {
+        collection.stream().on('data', function (task) {
+          var deferredTask = $q.defer();
+          client.tasks.findById(task.id)
+            .then(function (taskDetails) {
+              deferredTask.resolve(taskDetails)
+              workspaceTasks.push(taskDetails)
+            })      
+        }).on('end', function () {
+          deferredCollection.resolve(workspaceTasks)
+        })
       })
+      return deferredCollection.promise;
+    })
+
+    $q.all(promises).then(function (allTasksByWorkspace) {
+      console.log("allTasksByWorkspace:", allTasksByWorkspace);
+      var allTasks = _.flatten(allTasksByWorkspace)
+      console.log("allTasks:", allTasks);     
+      var allCompletedAt = allTasks.map(function(task){
+        return task.completed_at
+      }).filter(function(completed_at){ return !!completed_at })
+      console.log('allCompletedAt',allCompletedAt)
     })
   })
 
