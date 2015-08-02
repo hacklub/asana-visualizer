f3c44f9c70b84d71d9f8735628f29e82a91571b6
Mike Luby <mLuby@users.noreply.github.com>
2015-08-02 11:17:48 -0700

    switch to nvd3

--- client/app.js
+++ client/app.js
@@ -22,39 +22,75 @@ function dayChart(){
   return {
     restrict:'A'
     ,scope:{ data:'@' }
-    ,template:'<div class="ct-chart"></div><div>{{data}}</div>'
-    ,link:createChartist
+    ,template:'<div id="chart"><svg></svg></div><div class="data">{{data}}</div>'
+    ,link:createChart
   }
+  function createChart(){
+    console.log('discreteBarChart', typeof nv.models.discreteBarChart);
+    console.log('staggerLabels', typeof nv.models.discreteBarChart().staggerLabels);
+    console.log('tooltips', typeof nv.models.discreteBarChart().tooltips);
+    console.log('showValues', typeof nv.models.discreteBarChart().showValues);
+    console.log('transitionDuration', typeof nv.models.discreteBarChart().transitionDuration);
 
-  function createChartist(scope){
-    var datapoints = JSON.parse(scope.data);
-    // Create a simple bi-polar bar chart
-    var chartistData = {
-      labels: ['4am','5am','6am','7am','8am','9am','10am','11am','12 noon','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm','12 midnight','1am','2am','3am']
-      ,series: [ datapoints ]
-    };
+    nv.addGraph(function() {
+      var chart = nv.models.discreteBarChart()
+          .x(function(d) { return d.label })    //Specify the data accessors.
+          .y(function(d) { return d.value })
+          .staggerLabels(true)    //Too many bars and not enough room? Try staggering labels.
+          .tooltips(false)        //Don't show tooltips
+          .showValues(true)       //...instead, show the bar value right on top of each bar.
+          // .transitionDuration(350);
 
-    var chartistOptions = {
-      width: (2/3)*Math.max(document.documentElement.clientWidth, window.innerWidth || 0)
-      ,height: (2/3)*Math.max(document.documentElement.clientHeight, window.innerHeight || 0)
-      ,high: Math.max.apply(null, datapoints)+1
-      ,low: 0
-      ,axisX: { labelInterpolationFnc: function(value, index) { return (index-2) % 3 === 0 ? value : null; } }
-      ,axisY: { labelInterpolationFnc: function(value, index) { return index % 2 === 0 ? value : null; } }
-    };
+      d3.select('#chart svg')
+          .datum(exampleData())
+          .call(chart);
 
-    var chart = new Chartist.Bar('.ct-chart', chartistData, chartistOptions);
-    // Listen for draw events on the bar chart
-    chart.on('draw', function(data) {
-      // If this draw event is of type bar we can use the data to create additional content
-      if(data.type === 'bar') {
-        // We use the group element of the current series to append a simple circle with the bar peek coordinates and a circle radius that is depending on the value
-        data.group.append(new Chartist.Svg('circle', {
-          cx: data.x2,
-          cy: data.y2,
-          r: Math.abs(Chartist.getMultiValue(data.value)) * 2 + 5
-        }, 'ct-slice-pie'));
-      }
+      nv.utils.windowResize(chart.update);
+
+      return chart;
     });
+
+    //Each bar represents a single discrete quantity.
+    function exampleData() {
+     return  [
+        {
+          key: "Cumulative Return",
+          values: [
+            {
+              "label" : "A Label" ,
+              "value" : -29.765957771107
+            } ,
+            {
+              "label" : "B Label" ,
+              "value" : 0
+            } ,
+            {
+              "label" : "C Label" ,
+              "value" : 32.807804682612
+            } ,
+            {
+              "label" : "D Label" ,
+              "value" : 196.45946739256
+            } ,
+            {
+              "label" : "E Label" ,
+              "value" : 0.19434030906893
+            } ,
+            {
+              "label" : "F Label" ,
+              "value" : -98.079782601442
+            } ,
+            {
+              "label" : "G Label" ,
+              "value" : -13.925743130903
+            } ,
+            {
+              "label" : "H Label" ,
+              "value" : -5.1387322875705
+            }
+          ]
+        }
+      ]
+    }
   }
 }
--- client/timeService.js
+++ client/timeService.js
@@ -4,7 +4,7 @@ app.factory('timeService', function () {
     var rawTime = _.map(arrayOfTimes, function (time) {
       return moment(time).format('hA');
     });
-    
+
     var x= _.reduce(['4AM','5AM','6AM','7AM','8AM','9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM','8PM','9PM','10PM','11PM','12AM','1AM','2AM','3AM'], function (agg, time) {
       var y =_.reduce(rawTime, function countMatches(count, completedAtHour){
         return count += completedAtHour === time ? 1 : 0;
