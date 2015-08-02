3377c02f20ddffaee340ddf0d6455b03205ca80f
Mike Luby <mLuby@users.noreply.github.com>
2015-08-02 11:01:14 -0700

    data updates live

--- client/asanaService.js
+++ client/asanaService.js
@@ -1,16 +1,31 @@
-app.factory('asanaService', function ($q, timeService) {
+app.factory('asanaService', function ($q, timeService, $rootScope) {
+  var self = this;
   this.data = {
-    tasks: []
+    tasks: [],
+    taskCountByHourArr: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
   };
   // get token from cookie
   var token = document.cookie.split('token=')[1].split(';')[0]
   console.log('token:',token);
 
+  function updateTasksAndDigest(taskDetails){
+    console.log('adding',taskDetails,'to',self.data.tasks);
+    var hour = Number(taskDetails.split('T')[1].split(':')[0]);
+    var count = self.data.taskCountByHourArr[hour] + 1;
+    self.data.taskCountByHourArr[hour] = count;
+    // angular.copy(self.data.taskCountByHourArr, self.data.taskCountByHourArr);
+    $rootScope.$digest();
+  }
+
   if(token === 'test_token'){
     var allCompletedAt = ["2015-08-02T00:22:37.110Z", "2015-08-02T00:22:42.034Z", "2015-08-02T00:22:44.530Z", "2015-08-02T00:22:46.213Z", "2015-08-02T00:22:39.606Z", "2015-08-01T19:17:16.223Z", "2015-08-01T19:17:17.920Z", "2015-08-01T19:17:20.764Z", "2015-08-01T19:17:21.681Z", "2015-08-01T19:17:23.948Z", "2015-08-01T19:17:27.066Z"];
-    console.log("allCompletedAt: ", allCompletedAt);
-    // this.data.tasks.concat(allCompletedAt);
-    angular.copy(this.data.tasks, ['hi', 'there']);
+    var interval = setInterval(function(){
+      if(allCompletedAt.length > 0){
+        updateTasksAndDigest(allCompletedAt.pop());
+      } else {
+        clearInterval(interval);
+      }
+    }, 500);
   } else {
     var client = Asana.Client.create()
     client.useOauth({ credentials: token })
@@ -40,6 +55,7 @@ app.factory('asanaService', function ($q, timeService) {
               .then(function (taskDetails) {
                 deferredTask.resolve(taskDetails)
                 workspaceTasks.push(taskDetails)
+                updateTasksAndDigest(taskDetails);
               })
           }).on('end', function () {
             deferredCollection.resolve(workspaceTasks)
