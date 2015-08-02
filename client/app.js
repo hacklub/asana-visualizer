75087ac4547c27368a202b41e3e4c931ff65575b
Mike Luby <mLuby@users.noreply.github.com>
2015-08-02 13:56:46 -0700

    add timezone support fix #12

--- client/app.js
+++ client/app.js
@@ -3,5 +3,6 @@ var app = angular.module('Motivatr', ['nvd3']);
 app.controller('vizCtrl', vizCtrl);
 vizCtrl.$inject = ['$scope', 'asanaService', 'timeService'];
 function vizCtrl($scope, asanaService, timeService){
+  this.asana = asanaService;
   this.chart = asanaService.chart;
 };
