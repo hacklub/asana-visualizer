70c58b8d49fd4a3efbd9b817f2224f93b6018cdf
Ryn830 <ryn830@gmail.com>
2015-08-15 14:46:48 -0700

    (feat) Blinks text till all data is loaded

--- client/app.js
+++ client/app.js
@@ -3,8 +3,18 @@ var app = angular.module('Motivatr', ['nvd3']);
 app.controller('vizCtrl', vizCtrl);
 vizCtrl.$inject = ['$scope', 'asanaService', 'timeService'];
 function vizCtrl($scope, asanaService, timeService){
+  var self = this;
   this.asana = asanaService;
   this.chart = asanaService.chart;
+  this.blink = 'blink';
+  $scope.$watch(function () {
+    return asanaService.status;
+  }, function (status) {
+    if (status === 'done') {
+      self.blink = 'blinkOff';
+    } 
+  });
+
   this.findMostProductiveHour = function(chartData){
     var mostProductiveHour = chartData[0].values.reduce(function(max, datapoint){
       max = max.value > datapoint.value ? max : datapoint;
